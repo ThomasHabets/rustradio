06589ec0b37678d8ee1ecb8a6f77c5fde9f3c850
Thomas Habets <thomas@habets.se>
2023-09-12 21:51:21 +0100

    Move commented out general convert code to convert.rs

--- src/lib.rs
+++ src/lib.rs
@@ -86,35 +86,3 @@ impl<T> Stream<T> {
         }
     }
 }
-
-/*
-struct Convert<From, To> {
-    scale_from: From,
-    scale_to: To,
-}
-impl std::convert::Into<u32> for Float {
-    fn into(t: Float) -> u32 {
-        t as u32
-    }
-}
-impl<From, To> Convert<From, To>
-where From: std::ops::Mul<Output=From> + std::convert::TryInto<To>,
-      To: std::ops::Mul<Output=To>
-{
-    fn new(scale_from: From, scale_to: To) -> Self {
-        Self{
-            scale_from,
-            scale_to,
-        }
-    }
-    pub fn work(&mut self, r: &mut Stream<From>, w: &mut Stream<To>) -> Result<()>
-    where <From as TryInto<To>>::Error: std::fmt::Debug
-    {
-        let v = r.data.iter().map(|e| {
-            //From::into(*e * self.scale_from) * self.scale_to
-            (*e * self.scale_from).try_into().unwrap() * self.scale_to
-        });
-        Ok(())
-    }
-}
-*/
