abff595196fa46c605b9171fd371b1a2658cc094
Thomas Habets <thomas@habets.se>
2023-10-01 20:44:38 +0100

    Automate bumping version

--- extra/bump_version.sh
+++ extra/bump_version.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
+# Check that client is clean.
+[ -z "$(git status --porcelain)" ] || {
+    echo "git client not clean"
+    exit 1
+}
+
 CURRENT="$(awk '/^version/ {print $3}' Cargo.toml | sed 's/"//g')"
 NEW="$(echo $CURRENT | awk -F. '{print $1 "." $2 "." $3+1}')"
 echo "Current: '$CURRENT', New: '$NEW'"
 sed -i "s/^version = \"${CURRENT?}\"/version = \"${NEW?}\"/" Cargo.toml
-exec cargo build
+cargo build
+echo git commit -a -m"Bump version to ${NEW?}"
