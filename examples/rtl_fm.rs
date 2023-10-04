dbc321c1c41266ccd100b874a890d7d2bce5437f
Thomas Habets <thomas@habets.se>
2023-10-04 20:47:11 +0100

    examples/rtl_fm: A simple FM receiver

--- examples/rtl_fm.rs
+++ examples/rtl_fm.rs
@@ -1,40 +1,57 @@
-// Not even remotely working yet.
-
 use anyhow::Result;
 
-#[cfg(feature = "rtlsdr")]
-fn main() -> Result<()> {
-    println!("Hello world");
-    println!("Device count: {}", rtlsdr::get_device_count());
-    println!("Device name: {}", rtlsdr::get_device_name(0));
-    let ss = rtlsdr::get_device_usb_strings(0).unwrap();
-    println!(
-        "Manufacturer: {} Product: {} Serial: {}",
-        ss.manufacturer, ss.product, ss.serial
-    );
+use rustradio::blocks::*;
+use rustradio::file_sink::Mode;
+use rustradio::graph::Graph;
+use rustradio::stream::StreamType;
+use rustradio::Float;
 
-    let mut dev = rtlsdr::open(0).unwrap();
-    println!("Tuner type: {:?}", dev.get_tuner_type());
-    // dev.set_direct_sampling
-    dev.set_center_freq(868_000_000).unwrap();
-    println!("Allowed tuner gains: {:?}", dev.get_tuner_gains().unwrap());
-    dev.set_tuner_gain(27).unwrap();
-    println!("Tuner gain: {}", dev.get_tuner_gain());
-    let (xtal_clock_freq, xtal_tuner_freq) = dev.get_xtal_freq().unwrap();
-    println!("XTAL: {xtal_clock_freq} {xtal_tuner_freq}");
-    // dev.set_tuner_if_gain(…);
-    // dev.set_tuner_gain_mode
-    // dev.set_agc_mode
-    dev.set_sample_rate(1_024_000).unwrap();
-    //dev.
-    dev.reset_buffer().unwrap();
-    dev.read_sync(8192).expect("read to work");
-    println!("Done");
-    Ok(())
-}
-
-#[cfg(not(feature = "rtlsdr"))]
 fn main() -> Result<()> {
-    use rustradio::Error;
-    Err(Error::new("RTL SDR feature not enabled").into())
+    println!("Running rtl_fm example");
+    let mut g = Graph::new();
+
+    let freq = 100_000_000;
+    let samp_rate = 1024_000.0;
+    let igain = 20;
+
+    // RTL SDR source.
+    let src = g.add(Box::new(RtlSdrSource::new(freq, samp_rate as u32, igain)?));
+    let dec = g.add(Box::new(RtlSdrDecode::new()));
+    g.connect(StreamType::new_u8(), src, 0, dec, 0);
+
+    // Filter.
+    let taps = rustradio::fir::low_pass(samp_rate, 100_000.0, 1000.0);
+    let filter = g.add(Box::new(FftFilter::new(&taps)));
+    g.connect(StreamType::new_complex(), dec, 0, filter, 0);
+
+    // Resample.
+    let new_samp_rate = 200_000.0;
+    let resamp = g.add(Box::new(RationalResampler::new(
+        new_samp_rate as usize,
+        samp_rate as usize,
+    )?));
+    g.connect(StreamType::new_complex(), filter, 0, resamp, 0);
+    let samp_rate = new_samp_rate;
+
+    // Quad demod.
+    let quad = g.add(Box::new(QuadratureDemod::new(1.0)));
+    g.connect(StreamType::new_complex(), resamp, 0, quad, 0);
+
+    // Resample audio.
+    let new_samp_rate = 48_000.0;
+    let audio_resamp = g.add(Box::new(RationalResampler::new(
+        new_samp_rate as usize,
+        samp_rate as usize,
+    )?));
+    let _samp_rate = new_samp_rate;
+    g.connect(StreamType::new_float(), quad, 0, audio_resamp, 0);
+
+    // Save to file.
+    let sink = g.add(Box::new(FileSink::<Float>::new(
+        "test.f32",
+        Mode::Overwrite,
+    )?));
+    g.connect(StreamType::new_float(), audio_resamp, 0, sink, 0);
+
+    g.run()
 }
