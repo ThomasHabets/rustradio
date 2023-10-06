53f7417ffdf60d29cd735b00691018c79bc5a850
Thomas Habets <thomas@habets.se>
2023-10-07 00:23:56 +0100

    QuadratureDemod: speed up by 4x using feature fast-math

--- src/quadrature_demod.rs
+++ src/quadrature_demod.rs
@@ -1,7 +1,28 @@
-//! Quadrature demod, the core of an FM demodulator.
-/*
-TODO:
-* Look into https://mazzo.li/posts/vectorized-atan2.html
+/*! Quadrature demod, the core of an FM demodulator.
+
+Quadrature demodulation works is best done by thinking of the samples
+as vectors going out of the origin on the complex plane.
+
+A zero frequency means no "spinning" around the origin, but with all
+samples just being on a vector, with the same angle, though possibly
+varying magnitude.
+
+Negative frequency means the vector is spinning counter
+clockwise. Positive frequency means spinning clockwise.
+
+Quadrature demodulation discards the magnitude of the vector, and just
+looks at the angle between the current sample, and the previous
+sample.
+
+Because magnitude is discarded, this block is only useful for decoding
+frequency changes (FM, FSK, …), not things like QAM.
+
+[This article][vectorized] gives some good illustrations.
+
+Enabling the `fast-math` feature (dependency) speeds up
+QuadratureDemod by about 4x.
+
+[vectorized]: https://mazzo.li/posts/vectorized-atan2.html
 */
 use anyhow::Result;
 
@@ -27,7 +48,12 @@ impl QuadratureDemod {
     fn process_one(&mut self, s: Complex) -> Float {
         let t = s * self.last.conj();
         self.last = s;
-        self.gain * t.im.atan2(t.re)
+
+        #[cfg(feature = "fast-math")]
+        return self.gain * fast_math::atan2(t.im, t.re);
+
+        #[cfg(not(feature = "fast-math"))]
+        return self.gain * t.im.atan2(t.re);
     }
 }
 map_block_convert_macro![QuadratureDemod];
