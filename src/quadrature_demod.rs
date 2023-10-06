d602cdf8915717350403b55424b2f54ccde9f0f1
Thomas Habets <thomas@habets.se>
2023-10-06 22:34:53 +0100

    VOLK QuadratureDemod: use volk for conj mul too
    EVERYTHING IS THE SAME SPEED WTF?

--- src/quadrature_demod.rs
+++ src/quadrature_demod.rs
@@ -8,7 +8,11 @@ use log::info;
 
 use crate::block::{Block, BlockRet};
 use crate::stream::{InputStreams, OutputStreams, StreamType, Streamp};
-use crate::volk::volk_32fc_s32f_atan2_32f;
+use crate::volk::{
+    volk_32fc_s32f_atan2_32f,
+    volk_32fc_s32f_atan2_32f_b, // Special version.
+    volk_32fc_x2_multiply_conjugate_32fc,
+};
 use crate::{map_block_convert_macro, Complex, Error, Float};
 
 /// Quadrature demod, the core of an FM demodulator.
@@ -41,21 +45,38 @@ impl Block for QuadratureDemod {
     }
     fn work(&mut self, r: &mut InputStreams, w: &mut OutputStreams) -> Result<BlockRet, Error> {
         let i = r.get::<Complex>(0);
+        if i.borrow().available() == 0 {
+            return Ok(BlockRet::Ok);
+        }
+
         if false {
+            // This is the control. No volk here.
             w.get(0)
                 .borrow_mut()
                 .write(i.borrow().iter().map(|x| self.process_one(*x)));
         } else {
-            let mut idata = Vec::with_capacity(i.borrow().available() * 2);
-            //info!("Quad len: {}", idata.capacity());
+            // Volk code. Two parts. conjugate multiply, and then atan2.
+
+            // 1. Conjugate multiply.
+            let mut idata: Vec<Float> = Vec::with_capacity(i.borrow().available());
             for v in i.borrow().iter() {
-                let i = *v * self.last.conj();
-                idata.push(i.re);
-                idata.push(i.im);
-                self.last = *v;
+                idata.push(v.re);
+                idata.push(v.im);
             }
-            let v: Vec<Float> = crate::volk::volk_32fc_s32f_atan2_32f_b(&idata, self.gain);
-            //let v: Vec<Float>= idata.iter().map(|x| self.process_one(*x)).collect();
+
+            // 2. atan2
+            let idata = volk_32fc_x2_multiply_conjugate_32fc(&idata);
+            let v: Vec<Float> = if true {
+                let nt = idata.len();
+                let mut v = Vec::with_capacity(nt / 2);
+                for n in (0..nt).step_by(2) {
+                    let t = Complex::new(idata[n], idata[n + 1]);
+                    v.push(self.gain * t.im.atan2(t.re));
+                }
+                v
+            } else {
+                volk_32fc_s32f_atan2_32f_b(&idata, self.gain)
+            };
             w.get(0).borrow_mut().write_slice(&v);
         }
         i.borrow_mut().clear();
--- src/volk.rs
+++ src/volk.rs
@@ -47,18 +47,18 @@ pub fn volk_32fc_s32f_atan2_32f_b(input: &[c_float], scale: Float) -> Vec<Float>
     };
     o
 }
-/*pub fn volk_32fc_x2_multiply_conjugate_32fc(input: &[c_float]) -> Vec<Complex> {
-    let n = input.len() / 2;
-    let mut o: Vec<c_float> = Vec::with_capacity();
+
+pub fn volk_32fc_x2_multiply_conjugate_32fc(input: &[Float]) -> Vec<Float> {
+    let n = input.len();
+    let mut o: Vec<c_float> = Vec::with_capacity(n);
     unsafe {
-        volk_ffi::volk_32fc_s32f_atan2_32f(
+        volk_ffi::volk_32fc_x2_multiply_conjugate_32fc(
             o.as_mut_ptr(),
             input.as_ptr(),
-            scale as c_float,
-            n as c_uint,
+            input[2..].as_ptr(),
+            ((n / 2) - 1) as c_uint,
         );
-
+        o.set_len(n);
     };
     o
 }
-*/
