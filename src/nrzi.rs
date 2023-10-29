9ac6882cc614da31ded97fe40111fb28248a926f
Thomas Habets <thomas@habets.se>
2023-10-29 17:54:36 +0000

    Nrzi: Add more doc comments

--- src/nrzi.rs
+++ src/nrzi.rs
@@ -1,6 +1,8 @@
 /*! NRZI — Non return to zero
 
-The same effect can be had by doing:
+<https://en.wikipedia.org/wiki/Non-return-to-zero>
+
+The same effect as NrziDecode can be had by doing:
 
 ```text
 let (a, b) = add_block![g, Tee::new(prev)];
@@ -8,6 +10,11 @@ let delay = add_block![g, Delay::new(a, 1)];
 let prev = add_block![g, Xor::new(delay, b)];
 let prev = add_block![g, XorConst::new(prev, 1u8)];
 ```
+
+"NRZI" is actually ambiguous as to which is zero and which is
+one. This code is going with NRZI-S, meaning a toggle is zero, and
+constant is one, because that's what done by AX.25, both 1200bps Bell
+202, and 9600 G3RUH.
 */
 use crate::map_block_convert_macro;
 use crate::stream::{new_streamp, Streamp};
