b59efc9f74973ab78194d775c1b8a80f1440ec88
Thomas Habets <thomas@habets.se>
2023-10-31 22:18:01 +0000

    examples/ax25-1200-rx: Allow running against audio input, not just I/Q
    Decodes from standard test CD:
    * Track 1: 745
    * Track 2: 0
    * Track 3: 100
    * Track 4: 58
    
    Example for track 1:
    
    ```
    $ cargo run --release -F fast-math --example ax25-1200-rx -- -a -r cd/cd_tracks_01.au --sample_rate 44100  --out packets/
    Running…
    Block name         Seconds  Percent
    -----------------------------------
    FileSource           0.459    4.19%
    AuDecode             0.099    0.91%
    Hilbert              8.656   79.00%
    QuadratureDemod      0.433    3.95%
    FftFilterFloat       0.609    5.55%
    AddConst<T>          0.095    0.87%
    ZeroCrossing         0.388    3.54%
    BinarySlicer         0.002    0.02%
    NrziDecode           0.002    0.02%
    HDLC Deframer        0.016    0.15%
    PDU Writer           0.197    1.80%
    -----------------------------------
    All blocks          10.957   99.99%
    Non-block time       0.001    0.01%
    Elapsed seconds     10.959  100.00%
    ```
    
    As a side note, the test CD can be found at
    http://wa8lmf.net/TNCtest/index.htm, and can be converted from cue/bin
    to .wav and mono .au using:
    
    ```
    bchunk -w TNC_Test_Ver-1.1.bin TNC_Test_Ver-1.1.cue cd_tracks_
    for i in $(seq 7); do time sox cd_tracks_0${i}.wav -c 1 cd_tracks_0${i}.au ;done
    ```

--- examples/ax25-1200-rx.rs
+++ examples/ax25-1200-rx.rs
@@ -10,6 +10,14 @@ $ ./ax25-1200-rx -r captured.c32 --samp_rate 50000 -o captured
 $ ./ax25-1200-rx --rtlsdr -o captured -v 2
 […]
 ```
+
+I should run this against <http://wa8lmf.net/TNCtest/index.htm>, and
+read
+<https://github.com/wb2osz/direwolf/raw/master/doc/A-Better-APRS-Packet-Demodulator-Part-1-1200-baud.pdf>
+https://github.com/wb2osz/direwolf/raw/master/doc/WA8LMF-TNC-Test-CD-Results.pdf
+https://www.febo.com/packet/layer-one/transmit.html
+https://www.febo.com/packet/layer-one/receive.html
+
 */
 use std::path::PathBuf;
 
@@ -23,6 +31,9 @@ use rustradio::{Complex, Float};
 #[derive(StructOpt, Debug)]
 #[structopt()]
 struct Opt {
+    #[structopt(long = "audio", short = "a")]
+    audio: bool,
+
     #[structopt(long = "out", short = "o")]
     output: PathBuf,
 
@@ -30,21 +41,21 @@ struct Opt {
     #[structopt(long = "freq", default_value = "144800000")]
     freq: u64,
 
+    #[cfg(feature = "rtlsdr")]
+    #[structopt(long = "gain", default_value = "20")]
+    gain: i32,
+
     #[structopt(short = "v", default_value = "0")]
     verbose: usize,
 
     #[structopt(long = "rtlsdr")]
     rtlsdr: bool,
 
-    #[structopt(long = "samp_rate", default_value = "300000")]
+    #[structopt(long = "sample_rate", default_value = "300000")]
     samp_rate: u32,
 
     #[structopt(short = "r")]
     read: Option<String>,
-
-    #[cfg(feature = "rtlsdr")]
-    #[structopt(long = "gain", default_value = "20")]
-    gain: i32,
 }
 
 macro_rules! add_block {
@@ -68,49 +79,62 @@ fn main() -> Result<()> {
 
     let mut g = Graph::new();
 
-    let (prev, samp_rate) = if let Some(read) = opt.read {
-        let prev = add_block![g, FileSource::<Complex>::new(&read, false)?];
-        (prev, opt.samp_rate as Float)
-    } else if opt.rtlsdr {
-        #[cfg(feature = "rtlsdr")]
-        {
-            // Source.
-            let prev = add_block![g, RtlSdrSource::new(opt.freq, opt.samp_rate, opt.gain)?];
-
-            // Decode.
-            let prev = add_block![g, RtlSdrDecode::new(prev)];
+    // TODO: this is a complete mess.
+    let (prev, samp_rate) = if opt.audio {
+        if let Some(read) = opt.read {
+            let prev = add_block![g, FileSource::new(&read, false)?];
+            let prev = add_block![g, AuDecode::new(prev)];
             (prev, opt.samp_rate as Float)
+        } else {
+            panic!("Audio can only be read from file")
         }
-        #[cfg(not(feature = "rtlsdr"))]
-        panic!("rtlsdr feature not enabled")
     } else {
-        panic!("Need to provide either --rtlsdr or -r")
+        let prev = if let Some(read) = opt.read {
+            let prev = add_block![g, FileSource::<Complex>::new(&read, false)?];
+            prev
+        } else if opt.rtlsdr {
+            #[cfg(feature = "rtlsdr")]
+            {
+                // Source.
+                let prev = add_block![g, RtlSdrSource::new(opt.freq, opt.samp_rate, opt.gain)?];
+
+                // Decode.
+                let prev = add_block![g, RtlSdrDecode::new(prev)];
+                prev
+            }
+            #[cfg(not(feature = "rtlsdr"))]
+            panic!("rtlsdr feature not enabled")
+        } else {
+            panic!("Need to provide either --rtlsdr or -r")
+        };
+        let samp_rate = opt.samp_rate as Float;
+
+        // Filter RF.
+        let taps = rustradio::fir::low_pass_complex(samp_rate, 20_000.0, 100.0);
+        let prev = add_block![g, FftFilter::new(prev, &taps)];
+
+        // Resample RF.
+        let new_samp_rate = 50_000.0;
+        let prev = add_block![
+            g,
+            RationalResampler::new(prev, new_samp_rate as usize, samp_rate as usize)?
+        ];
+        let samp_rate = new_samp_rate;
+
+        // Save I/Q to file.
+        /*
+            let (prev, b) = add_block![g, Tee::new(prev)];
+            g.add(Box::new(FileSink::new(
+            b,
+            "test.c32",
+            rustradio::file_sink::Mode::Overwrite,
+        )?));
+             */
+
+        // TODO: AGC step?
+        let prev = add_block![g, QuadratureDemod::new(prev, 1.0)];
+        (prev, samp_rate)
     };
-
-    // Filter RF.
-    let taps = rustradio::fir::low_pass_complex(samp_rate, 20_000.0, 100.0);
-    let prev = add_block![g, FftFilter::new(prev, &taps)];
-
-    // Resample RF.
-    let new_samp_rate = 50_000.0;
-    let prev = add_block![
-        g,
-        RationalResampler::new(prev, new_samp_rate as usize, samp_rate as usize)?
-    ];
-    let samp_rate = new_samp_rate;
-
-    // Save I/Q to file.
-    /*
-    let (prev, b) = add_block![g, Tee::new(prev)];
-    g.add(Box::new(FileSink::new(
-        b,
-        "test.c32",
-        rustradio::file_sink::Mode::Overwrite,
-    )?));
-     */
-
-    // TODO: AGC step?
-    let prev = add_block![g, QuadratureDemod::new(prev, 1.0)];
     let prev = add_block![g, Hilbert::new(prev, 65)];
     let prev = add_block![g, QuadratureDemod::new(prev, 1.0)];
 
@@ -134,7 +158,6 @@ fn main() -> Result<()> {
         rustradio::file_sink::Mode::Overwrite,
     )?));
      */
-
     let baud = 1200.0;
     let prev = add_block![g, ZeroCrossing::new(prev, samp_rate / baud, 0.1)];
     let prev = add_block![g, BinarySlicer::new(prev)];
@@ -169,3 +192,8 @@ fn main() -> Result<()> {
     eprintln!("{}", g.generate_stats(st.elapsed()));
     Ok(())
 }
+/* ---- Emacs variables ----
+ * Local variables:
+ * compile-command: "cargo run --example ax25-1200-rx -- -r ../aprs-50k.c32 --sample_rate 50000 -o ../packets"
+ * End:
+ */
