593a5d6a91187ccef70dba794ad2e159d3af72ce
Thomas Habets <thomas@habets.se>
2023-11-14 20:34:09 +0000

    examples/simple_graph: Add structopt

--- examples/simple_graph.rs
+++ examples/simple_graph.rs
@@ -1,8 +1,16 @@
 use anyhow::Result;
+use structopt::StructOpt;
 
 use rustradio::blocks::{AddConst, DebugSink, PduWriter, StreamToPdu, VectorSourceBuilder};
 use rustradio::graph::Graph;
 use rustradio::Complex;
+
+#[derive(StructOpt, Debug)]
+#[structopt()]
+struct Opt {
+    #[structopt(short = "v", default_value = "0")]
+    verbose: usize,
+}
 
 fn simple_copy() -> Result<()> {
     let mut g = Graph::new();
@@ -51,6 +59,15 @@ fn simple_noncopy() -> Result<()> {
 }
 
 fn main() -> Result<()> {
+    println!("Simple test graphs");
+    let opt = Opt::from_args();
+    stderrlog::new()
+        .module(module_path!())
+        .module("rustradio")
+        .quiet(false)
+        .verbosity(opt.verbose)
+        .timestamp(stderrlog::Timestamp::Second)
+        .init()?;
     simple_copy()?;
     simple_noncopy()
 }
