1cd678280047e08d9d999b80fb9cc48d6d8a3e1f
Thomas Habets <thomas@habets.se>
2023-12-11 20:54:47 +0000

    Benchmarks: Switch to criterion framework

--- benches/bench_rustradio.rs
+++ benches/bench_rustradio.rs
@@ -1,78 +1,80 @@
-#![feature(test)]
-
 extern crate rustradio;
-extern crate test;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
 use rustradio::block::Block;
 use rustradio::blocks::*;
-use rustradio::stream::new_streamp;
-use rustradio::Complex;
+use rustradio::stream::{new_streamp, Streamp};
 
-use test::Bencher;
+fn setup_empty<T: Copy, T2: Copy>(s: &Streamp<T>, so: &Streamp<T2>) {
+    let len = s.len();
+    // Empty input buffer.
+    {
+        let (i, _) = s.read_buf().expect("getting read buffer");
+        let n = i.len();
+        i.consume(n);
+    }
+    // Fill input buffer.
+    {
+        let o = s.write_buf().expect("getting read buffer");
+        o.produce(len, &[]);
+    }
+    // Empty output buffer.
+    {
+        let obind = so;
+        let (out, _) = obind.read_buf().expect("getting write buffer");
+        let n = out.len();
+        out.consume(n);
+    }
+}
 
-#[bench]
-fn bench_fft_filter(b: &mut Bencher) {
-    let taps = rustradio::fir::low_pass_complex(1024000.0, 50000.0, 10000.0);
-    let input = {
-        let mut a = taps.clone();
-        a.resize(a.len() * 2, Complex::default());
-        a
-    };
-    let s = new_streamp();
-    let mut filter = FftFilter::new(s.clone(), &taps);
-    b.iter(|| {
-        // Empty input buffer.
-        {
-            let (i, _) = s.read_buf().unwrap();
-            let n = i.len();
-            i.consume(n);
-        }
-        // Fill input buffer.
-        {
-            let o = s.write_buf().unwrap();
-            //o.slice()[..input.len()].clone_from_slice(&input);
-            o.produce(input.len(), &[]);
-        }
-        // Empty output buffer.
-        {
-            let obind = filter.out();
-            let (out, _) = obind.read_buf().unwrap();
-            let n = out.len();
-            out.consume(n);
-        }
-        filter.work().unwrap();
+fn run_simple<Tin: Copy, Tout: Copy>(
+    c: &mut Criterion,
+    s: Streamp<Tin>,
+    so: Streamp<Tout>,
+    block: &mut dyn Block,
+) {
+    c.bench_function(block.block_name(), |b| {
+        b.iter(|| {
+            setup_empty(&s, &so);
+            let st = std::time::Instant::now();
+            block.work().unwrap();
+            st.elapsed()
+        })
     });
 }
 
-#[bench]
-fn bench_fir_filter(b: &mut Bencher) {
-    let taps = rustradio::fir::low_pass_complex(1024000.0, 50000.0, 10000.0);
-    let input = {
-        let mut a = taps.clone();
-        a.resize(a.len() * 2, Complex::default());
-        a
-    };
+fn bench_fast_fm(c: &mut Criterion) {
     let s = new_streamp();
-    let mut filter = FIRFilter::new(s.clone(), &taps);
-    b.iter(|| {
-        // Empty input buffer.
-        {
-            let (i, _) = s.read_buf().unwrap();
-            let n = i.len();
-            i.consume(n);
-        }
-        // Fill input buffer.
-        {
-            let o = s.write_buf().unwrap();
-            //o.slice()[..input.len()].clone_from_slice(&input);
-            o.produce(input.len(), &[]);
-        }
-        // Empty output buffer.
-        {
-            let obind = filter.out();
-            let (out, _) = obind.read_buf().unwrap();
-            let n = out.len();
-            out.consume(n);
-        }
-        filter.work().unwrap();
-    });
+    let mut block = FastFM::new(s.clone());
+    run_simple(c, s, block.out(), &mut block);
 }
+
+fn bench_quad_demod(c: &mut Criterion) {
+    let s = new_streamp();
+    let mut block = QuadratureDemod::new(s.clone(), 1.0);
+    run_simple(c, s, block.out(), &mut block);
+}
+
+fn bench_fft_filter(c: &mut Criterion) {
+    let taps = rustradio::fir::low_pass_complex(1024000.0, 50000.0, 10000.0);
+    let s = new_streamp();
+    let mut block = FftFilter::new(s.clone(), &taps);
+    run_simple(c, s, block.out(), &mut block);
+}
+
+fn bench_fir_filter(c: &mut Criterion) {
+    let taps = rustradio::fir::low_pass_complex(1024000.0, 50000.0, 10000.0);
+    let s = new_streamp();
+    let mut block = FIRFilter::new(s.clone(), &taps);
+    run_simple(c, s, block.out(), &mut block);
+}
+
+criterion_group!(
+    benches,
+    bench_quad_demod,
+    bench_fast_fm,
+    bench_fft_filter,
+    bench_fir_filter,
+);
+criterion_main!(benches);
