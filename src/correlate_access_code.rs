335170c8897306c02c595bd05fc755132aa9cc12
Thomas Habets <thomas@habets.se>
2024-01-20 21:32:35 +0000

    CAC: Optionally allow some diffs

--- src/correlate_access_code.rs
+++ src/correlate_access_code.rs
@@ -11,24 +11,33 @@ pub struct CorrelateAccessCode {
     src: Streamp<u8>,
     dst: Streamp<u8>,
     slide: Vec<u8>,
+    allowed_diffs: usize,
 }
 
 impl CorrelateAccessCode {
     /// Create new correlate access block.
-    pub fn new(src: Streamp<u8>, code: Vec<u8>) -> Self {
+    pub fn new(src: Streamp<u8>, code: Vec<u8>, allowed_diffs: usize) -> Self {
         Self {
             src,
             code,
             dst: new_streamp(),
             slide: Vec::new(),
+            allowed_diffs,
         }
     }
     fn process_one(&mut self, a: u8) -> u8 {
         self.slide.push(a);
+
         if self.slide.len() > self.code.len() {
             self.slide.remove(0);
         }
-        if self.slide == self.code {
+        let diffs = self
+            .slide
+            .iter()
+            .zip(&self.code)
+            .filter(|(a, b)| a != b)
+            .count();
+        if diffs <= self.allowed_diffs {
             1
         } else {
             0
