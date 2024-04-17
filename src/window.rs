4f5552c115eba3f31c993c40f6866600b565dd32
Thomas Habets <thomas@habets.se>
2024-04-17 22:35:23 +0100

    window: Slight cleanup and commenting

--- src/window.rs
+++ src/window.rs
@@ -1,14 +1,25 @@
 //! Window functions
 //!
+//! All functions are periodic, not symmetric.(?)
+//!
 //! https://en.wikipedia.org/wiki/Window_function
+//! https://en.wikipedia.org/wiki/Spectral_leakage
+//!
+//! Notable missing window functions:
+//! * Hann
+//! * Rectangular
+//! * Triangular
+//! * Flat top
+//! * Gaussian
+//! * Taylor
 use crate::Float;
+
+const PI: Float = std::f64::consts::PI as Float;
 
 /// Create Hamming window.
 ///
 /// https://en.wikipedia.org/wiki/Window_function#Hann_and_Hamming_windows
 pub fn hamming(ntaps: usize) -> Vec<Float> {
-    let pi = std::f64::consts::PI as Float;
-
     // a0 notes:
     //
     // 0.54 is commonly used, but Hamming's paper sets it as 25/46.
@@ -17,11 +28,11 @@ pub fn hamming(ntaps: usize) -> Vec<Float> {
     // coefficients are a0 = 0.53836 and a1 = 0.46164".
     //
     // See wikipedia.
-    let a0 = 25.0 / 46.0; // 0.54 is commonly used.
+    let a0 = 25.0 / 46.0;
     let a1 = 1.0 - a0;
     let m = (ntaps - 1) as Float;
     (0..ntaps)
-        .map(|n| a0 - a1 * (2.0 * pi * (n as Float) / m).cos())
+        .map(|n| a0 - a1 * (2.0 * PI * (n as Float) / m).cos())
         .collect()
 }
 
@@ -33,19 +44,28 @@ pub fn blackman(m: usize) -> Vec<Float> {
     for n in 0..m {
         let n = n as Float;
         let m = m as Float;
-        let pi = std::f64::consts::PI as Float;
 
         // Blackman's "not very serious proposal" magic value: 0.16.
         let a = 0.16;
 
         // Parameters.
+        //
+        // "exact Blackman" is:
+        //   a0 = 7938/18608 ≈ 0.42659
+        //   a1 = 9240/18608 ≈ 0.49656
+        //   a2 = 1430/18608 ≈ 0.076849
+        //
+        // The truncated coefficients do not null the sidelobes as
+        // well, but have an improved 18 dB/oct fall-off (compared do
+        // 6dB for exact).
+
         let a0 = (1.0 - a) / 2.0;
         let a1 = 0.5;
         let a2 = a / 2.0;
 
         // Formula.
-        let t1 = 2.0 * pi * n / m;
-        let t2 = 4.0 * pi * n / m;
+        let t1 = 2.0 * PI * n / m;
+        let t2 = 4.0 * PI * n / m;
         b.push(a0 - a1 * t1.cos() + a2 * t2.cos());
     }
     b
@@ -59,19 +79,18 @@ pub fn blackman_harris(m: usize) -> Vec<Float> {
     for n in 0..m {
         let n = n as Float;
         let m = m as Float;
-        let pi = std::f64::consts::PI as Float;
 
         // Parameters.
-        let a0 = 0.35875;
-        let a1 = 0.48829;
-        let a2 = 0.14128;
-        let a3 = 0.01168;
+        const A0: Float = 0.35875;
+        const A1: Float = 0.48829;
+        const A2: Float = 0.14128;
+        const A3: Float = 0.01168;
 
         // Formula.
-        let t1 = 2.0 * pi * n / m;
-        let t2 = 4.0 * pi * n / m;
-        let t3 = 6.0 * pi * n / m;
-        b.push(a0 - a1 * t1.cos() + a2 * t2.cos() - a3 * t3.cos());
+        let t1 = 2.0 * PI * n / m;
+        let t2 = 4.0 * PI * n / m;
+        let t3 = 6.0 * PI * n / m;
+        b.push(A0 - A1 * t1.cos() + A2 * t2.cos() - A3 * t3.cos());
     }
     b
 }
