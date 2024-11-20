9027f5c2220e140740eceb7b917a768bd20864a8
Thomas Habets <thomas@habets.se>
2024-11-20 23:31:53 +0000

    Add: Add unit test

--- src/add.rs
+++ src/add.rs
@@ -27,3 +27,31 @@ where
         a + b
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::block::Block;
+    use crate::blocks::VectorSource;
+    use crate::Float;
+
+    #[test]
+    fn add_float() -> crate::Result<()> {
+        let input_a: Vec<_> = (0..10).map(|i| i as Float).collect();
+        let mut a = VectorSource::new(input_a);
+        a.work()?;
+
+        let input_b: Vec<_> = (0..20).map(|i| 2.0 * (i as Float)).collect();
+        let mut b = VectorSource::new(input_b);
+        b.work()?;
+
+        let mut add = Add::new(a.out(), b.out());
+        add.work()?;
+        let os = add.out();
+        let (res, _) = os.read_buf()?;
+        let want: Vec<_> = (0..10).map(|i| 3 * i).collect();
+        let got: Vec<_> = res.slice().iter().map(|f| *f as usize).collect();
+        assert_eq!(got, want);
+        Ok(())
+    }
+}
