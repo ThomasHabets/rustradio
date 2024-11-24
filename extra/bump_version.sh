437c9bc9d17fabed75fc782fde9640bf6f90db2a
Thomas Habets <thomas@habets.se>
2024-11-24 20:48:18 +0000

    bump_version: update rustradio_macros dep version

--- extra/bump_version.sh
+++ extra/bump_version.sh
@@ -11,6 +11,7 @@ CURRENT="$(awk '/^version/ {print $3}' Cargo.toml | sed 's/"//g')"
 NEW="$(echo $CURRENT | awk -F. '{print $1 "." $2 "." $3+1}')"
 echo "Current: '$CURRENT', New: '$NEW'"
 sed -i "s/^version = \"${CURRENT?}\"/version = \"${NEW?}\"/" Cargo.toml
+sed -i -r 's/^(rustradio_macros.*version = ")[0-9.]+(".*)/\1'"${NEW}"'\2/' Cargo.toml
 sed -i "s/^version = \"${CURRENT?}\"/version = \"${NEW?}\"/" rustradio_macros/Cargo.toml
 cargo build
 git commit -a -m"Bump version to ${NEW?}"
