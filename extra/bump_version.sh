f6eda3bd93f4859b26cb8c1e85d40194e1379b42
Thomas Habets <thomas@habets.se>
2024-11-24 12:32:22 +0000

    bump_version: Also bump macro crate

--- extra/bump_version.sh
+++ extra/bump_version.sh
@@ -11,6 +11,7 @@ CURRENT="$(awk '/^version/ {print $3}' Cargo.toml | sed 's/"//g')"
 NEW="$(echo $CURRENT | awk -F. '{print $1 "." $2 "." $3+1}')"
 echo "Current: '$CURRENT', New: '$NEW'"
 sed -i "s/^version = \"${CURRENT?}\"/version = \"${NEW?}\"/" Cargo.toml
+sed -i "s/^version = \"${CURRENT?}\"/version = \"${NEW?}\"/" rustradio_macros/Cargo.toml
 cargo build
 git commit -a -m"Bump version to ${NEW?}"
 git tag "v${NEW?}"
