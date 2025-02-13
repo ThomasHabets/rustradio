4348ec2903ff2680424cba4d39670861788e37b2
Thomas Habets <thomas@habets.se>
2025-02-13 20:57:34 +0000

    FftStream: Optionally multithread using rayon
    In my initial tests it doesn't seem to help.

--- src/fft_stream.rs
+++ src/fft_stream.rs
@@ -12,6 +12,7 @@ use crate::{Complex, Error, Float};
 pub struct FftStream {
     size: usize,
     fft: std::sync::Arc<dyn rustfft::Fft<Float>>,
+    threaded: bool,
     #[rustradio(in)]
     src: ReadStream<Complex>,
     #[rustradio(out)]
@@ -19,6 +20,7 @@ pub struct FftStream {
 }
 
 impl FftStream {
+    /// Create a new FftStream.
     pub fn new(src: ReadStream<Complex>, size: usize) -> (Self, ReadStream<Complex>) {
         let mut planner = FftPlanner::new();
         let fft = planner.plan_fft_forward(size);
@@ -29,9 +31,17 @@ impl FftStream {
                 fft,
                 src,
                 dst,
+                threaded: false,
             },
             dr,
         )
+    }
+    /// Turn on or off Rayon multithreading.
+    ///
+    /// Initial benchmarks seem to indicate that this does not help. Maybe with
+    /// bigger than default stream buffers for more concurrency.
+    pub fn threaded(&mut self, onoff: bool) {
+        self.threaded = onoff;
     }
 }
 
@@ -47,10 +57,26 @@ impl Block for FftStream {
         if oo.len() < self.size {
             return Ok(BlockRet::Ok);
         }
-        oo[..self.size].copy_from_slice(&ii[..self.size]);
-        self.fft.process(oo);
-        input.consume(self.size);
-        o.produce(self.size, &[]);
+        let len = std::cmp::min(ii.len(), oo.len());
+        let len = len - (len % self.size);
+        oo[..len].copy_from_slice(&ii[..len]);
+
+        // It would be nice to use fft.process_outofplace_with_scratch(), but it
+        // requires input also be scratch space, and therefore mutable.
+        if self.threaded {
+            use rayon::prelude::*;
+            oo.par_chunks_mut(self.size).for_each(|chunk| {
+                self.fft.process(chunk);
+            });
+        } else {
+            oo.chunks_mut(self.size).for_each(|chunk| {
+                self.fft.process(chunk);
+            });
+        }
+        input.consume(len);
+        o.produce(len, &[]);
         Ok(BlockRet::Ok)
     }
 }
+/* vim: textwidth=80
+ */
