3765ab641d86e003cb10738cb9eca838cb094cfd
Thomas Habets <thomas@habets.se>
2025-02-16 22:02:17 +0000

    Tee: Convert to sync block

--- src/xor.rs
+++ src/xor.rs
@@ -1,15 +1,13 @@
 //! Xor two streams.
-use crate::block::{Block, BlockRet};
 use crate::stream::{ReadStream, WriteStream};
-use crate::Error;
 
 /// Xors a constant value to every sample.
 // TODO: make this sync
 #[derive(rustradio_macros::Block)]
-#[rustradio(crate, new)]
+#[rustradio(crate, new, sync)]
 pub struct Xor<T>
 where
-    T: Copy,
+    T: Copy + std::ops::BitXor<Output = T>,
 {
     #[rustradio(in)]
     a: ReadStream<T>,
@@ -19,26 +17,11 @@ where
     dst: WriteStream<T>,
 }
 
-impl<T> Block for Xor<T>
+impl<T> Xor<T>
 where
     T: Copy + std::ops::BitXor<Output = T>,
 {
-    fn work(&mut self) -> Result<BlockRet, Error> {
-        let (a, tags) = self.a.read_buf()?;
-        let (b, _tags) = self.b.read_buf()?;
-        let n = std::cmp::min(a.len(), b.len());
-        if n == 0 {
-            return Ok(BlockRet::Noop);
-        }
-        let mut o = self.dst.write_buf()?;
-        let n = std::cmp::min(n, o.len());
-        let it = a.iter().zip(b.iter()).map(|(x, y)| *x ^ *y);
-        for (w, samp) in o.slice().iter_mut().take(n).zip(it) {
-            *w = samp;
-        }
-        a.consume(n);
-        b.consume(n);
-        o.produce(n, &tags);
-        Ok(BlockRet::Ok)
+    fn process_sync(&self, a: T, b: T) -> T {
+        a ^ b
     }
 }
