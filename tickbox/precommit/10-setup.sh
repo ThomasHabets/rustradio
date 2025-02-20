35f3c08075e2e1e425d695f16bd029853c10c1cc
Thomas Habets <thomas@habets.se>
2025-02-20 22:35:05 +0000

    tickbox: Fix tickboxes

--- tickbox/precommit/10-setup.sh
+++ tickbox/precommit/10-setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # TODO: Make his a feature of tickbox.
 set -ueo pipefail
+if [[ "${NODIFF:-}" = "true" ]]; then
+        ln -s "$(pwd)" "$TICKBOX_TEMPDIR/work"
+        exit 0
+fi
 mkdir "$TICKBOX_TEMPDIR/work"
 git archive HEAD | tar -x -C "$TICKBOX_TEMPDIR/work"
 git diff --cached --binary | (
--- tickbox/precommit/50-nightly-test.sh
+++ tickbox/precommit/50-nightly-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ueo pipefail
-if cargo +nightly 2> /dev/null > /dev/null && {
+cargo +nightly 2> /dev/null > /dev/null && {
         export CARGO_TARGET_DIR="$TICKBOX_CWD/target/${TICKBOX_BRANCH}.bench.nightly"
         cd "$TICKBOX_TEMPDIR/work"
         exec cargo +nightly bench --no-run -F rtlsdr
