90d9942c80c5bef1e135b22a120913766c1d5d9f
Thomas Habets <thomas@habets.se>
2025-02-21 23:58:43 +0000

    FloatToComplex: make sync

--- src/convert.rs
+++ src/convert.rs
@@ -1,9 +1,7 @@
 //! Blocks for converting from one type to another.
 use anyhow::Result;
 
-use crate::block::{Block, BlockRet};
 use crate::stream::{ReadStream, WriteStream};
-use crate::Error;
 use crate::{Complex, Float};
 
 /// Builder for Map.
@@ -90,9 +88,8 @@ where
 }
 
 /// Convert floats to complex.
-// TODO: make sync.
 #[derive(rustradio_macros::Block)]
-#[rustradio(crate, new)]
+#[rustradio(crate, new, sync)]
 pub struct FloatToComplex {
     #[rustradio(in)]
     re: ReadStream<Float>,
@@ -102,28 +99,8 @@ pub struct FloatToComplex {
     dst: WriteStream<Complex>,
 }
 
-impl Block for FloatToComplex {
-    fn work(&mut self) -> Result<BlockRet, Error> {
-        let (a, tags) = self.re.read_buf()?;
-        if a.is_empty() {
-            return Ok(BlockRet::WaitForStream(&self.re, 1));
-        }
-        let (b, _) = self.im.read_buf()?;
-        if b.is_empty() {
-            return Ok(BlockRet::WaitForStream(&self.im, 1));
-        }
-        let n = std::cmp::min(a.len(), b.len());
-        let mut o = self.dst.write_buf()?;
-        let n = std::cmp::min(n, o.len());
-        o.fill_from_iter(
-            a.iter()
-                .zip(b.iter())
-                .take(n)
-                .map(|(x, y)| Complex::new(*x, *y)),
-        );
-        a.consume(n);
-        b.consume(n);
-        o.produce(n, &tags);
-        Ok(BlockRet::Ok)
+impl FloatToComplex {
+    fn process_sync(&self, re: Float, im: Float) -> Complex {
+        Complex::new(re, im)
     }
 }
