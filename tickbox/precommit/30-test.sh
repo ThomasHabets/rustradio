f50dc524bedb31c2b17955671fe0cbbf1817f5b8
Thomas Habets <thomas@habets.se>
2025-03-01 00:58:17 +0000

    precommit: Improve cache use

--- tickbox/precommit/30-test.sh
+++ tickbox/precommit/30-test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -ueo pipefail
-export CARGO_TARGET_DIR="$TICKBOX_CWD/target/${TICKBOX_BRANCH}.test.normal"
 cd "$TICKBOX_TEMPDIR/work"
 for dir in . rustradio_macros; do
         (
+                export CARGO_TARGET_DIR="$TICKBOX_CWD/${dir}/target/${TICKBOX_BRANCH}.test.normal"
                 cd "$dir"
                 cargo test
         )
--- tickbox/precommit/70-nightly-clippy.sh
+++ tickbox/precommit/70-nightly-clippy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -ueo pipefail
-export CARGO_TARGET_DIR="$TICKBOX_CWD/target/${TICKBOX_BRANCH}.clippy"
 cd "$TICKBOX_TEMPDIR/work"
 for dir in . rustradio_macros; do
         (
+                export CARGO_TARGET_DIR="$TICKBOX_CWD/${dir}/target/${TICKBOX_BRANCH}.clippy"
                 cd "$dir"
                 cargo clippy -- -D warnings
         )
