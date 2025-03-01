9a0e01608496206ddeadc96cc527ba777bc600c2
Thomas Habets <thomas@habets.se>
2025-03-01 00:50:41 +0000

    precommit: Give clippy-ing its own dir

--- tickbox/precommit/70-nightly-clippy.sh
+++ tickbox/precommit/70-nightly-clippy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -ueo pipefail
+export CARGO_TARGET_DIR="$TICKBOX_CWD/target/${TICKBOX_BRANCH}.clippy"
 cd "$TICKBOX_TEMPDIR/work"
 for dir in . rustradio_macros; do
         (
