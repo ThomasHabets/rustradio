eb77b1db1d1f33b6e7de5811d4e57bbe7bc1604a
Thomas Habets <thomas@habets.se>
2025-03-03 22:38:37 +0000

    Block: Improve docstrings

--- src/block.rs
+++ src/block.rs
@@ -1,22 +1,16 @@
-/*! RustRadio Block implementation
-
-Blocks are the main building blocks of rustradio. They each do one
-thing, and you connect them together with streams to process the data.
-
-*/
-
+//! RustRadio Block implementation
+//!
+//! Blocks are the main building blocks of rustradio. They each do one
+//! thing, and you connect them together with streams to process the data.
 use anyhow::Result;
 
 use crate::Error;
 use crate::stream::StreamWait;
 
-/** Return type for all blocks.
-
-This will let the scheduler know if more data could come out of this block, or if
-it should just never bother calling it again.
-
-TODO: Add state for "don't call me unless there's more input".
- */
+/// Return type for all blocks.
+///
+/// This will let the scheduler know if more data could come out of this block,
+/// or if it should just never bother calling it again.
 //#[derive(Debug)]
 pub enum BlockRet<'a> {
     /// Everything is fine, but no information about when more data could be
@@ -24,6 +18,23 @@ pub enum BlockRet<'a> {
     ///
     /// The graph scheduler should feel free to call the `work` function again
     /// without waiting or sleeping.
+    ///
+    /// Ok should not be returned for "polling". In other words, it should not
+    /// be returned repeatedly without data being consumed or produced.
+    ///
+    /// Good examples of returning Ok:
+    /// * A block finished being in a state (e.g. writing headers), and does not
+    ///   want to deal with restarting `work()` under the new state. Next time
+    ///   `work()` is called, it'll be in a new state, so it's just temporary.
+    ///   Example `AuEncode`.
+    /// * Stream status is checked at the start of `work()`, so instead of
+    ///   re-checking status after a `produce()`/`consume()`, it's easier
+    ///   to just let the graph call `work()` again.
+    ///   Examples: `RtlSdrDecode` and `FirFilter`.
+    ///
+    /// Importantly, in both these examples, a second `work()` call is not
+    /// expected to do nothing, and just return `Ok`. It'll either do useful
+    /// work, or it'll properly return a status showing what it's blocked on.
     Ok,
 
     /// Block didn't produce anything this time, but has a background
@@ -32,6 +43,9 @@ pub enum BlockRet<'a> {
     /// The difference between `Ok` and `Pending` is that `Pending` implies to
     /// the graph runner that it's reasonable to sleep a bit before calling
     /// `work` again. And that activity on any stream won't help either way.
+    ///
+    /// Example: `RtlSdrSource` may not currently have any new data, but we
+    /// can't control when it does.
     Pending,
 
     /// Block indicates that there's no point calling it until the provided
@@ -41,7 +55,9 @@ pub enum BlockRet<'a> {
     /// prevents checking for exit conditions like stream EOF and Ctrl-C.
     ///
     /// For a single threaded graph, it would stall all blocks, so it's not
-    /// used.
+    /// called at all, and thus becomes equivalent to returning `Ok`.
+    ///
+    /// Discouraged: Prefer WaitForStream when possible.
     WaitForFunc(Box<dyn Fn() + 'a>),
 
     /// Signal that we're waiting for a stream. Either an input or output
@@ -56,11 +72,16 @@ pub enum BlockRet<'a> {
     /// Block indicates that it will never produce more input.
     ///
     /// Examples:
-    /// * reading from file, without repeating, and file reached EOF.
+    /// * Reading from file, without repeating, and file reached EOF.
+    /// * Reading from a `VectorSource` that reached its end.
     /// * Head block reached its max.
     EOF,
 }
 
+/// Provide name of block.
+///
+/// This has to be a separate trait, because often the `impl` is proc macro
+/// generated, and it's not possible to re-open the same trait `impl` in Rust.
 pub trait BlockName {
     /// Name of block
     ///
@@ -69,6 +90,10 @@ pub trait BlockName {
     fn block_name(&self) -> &str;
 }
 
+/// Enable asking if a block is done, and will never return any more data.
+///
+/// This has to be a separate trait, because often the `impl` is proc macro
+/// generated, and it's not possible to re-open the same trait `impl` in Rust.
 pub trait BlockEOF {
     /// Return EOF status.
     ///
@@ -79,7 +104,7 @@ pub trait BlockEOF {
     }
 }
 
-/// Block trait, that must be implemented for all blocks.
+/// Block trait. Must be implemented for all blocks.
 ///
 /// Simpler blocks can use macros to avoid needing to implement `work()`.
 pub trait Block: BlockName + BlockEOF {
