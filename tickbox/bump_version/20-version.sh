e01c93b33778b0433aa3d799e3402941aff837da
Thomas Habets <thomas@habets.se>
2025-03-03 09:32:47 +0000

    tickbox/bump_version: Allow manual version override

--- tickbox/bump_version/20-version.sh
+++ tickbox/bump_version/20-version.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 CURRENT="$(awk '/^version/ {print $3}' Cargo.toml | head -1 | sed 's/"//g')"
 AUTO_NEW_VERSION="$(echo $CURRENT | awk -F. '{print $1 "." $2 "." $3+1}')"
 NEW="${1:-$AUTO_NEW_VERSION}"
+
+# MANUAL_VERSION is set manually by the person running the release.
+NEW="${MANUAL_VERSION:-$NEW}"
 echo "Current: '$CURRENT', New: '$NEW'"
 sed -i "s/^version = \"${CURRENT?}\"/version = \"${NEW?}\"/" Cargo.toml
 sed -i -r 's/^(rustradio_macros.*version = ")[0-9.]+(".*)/\1'"${NEW}"'\2/' Cargo.toml
