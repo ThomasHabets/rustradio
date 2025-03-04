a3c5b95548132638ce392dfaae9310bc68447e92
Thomas Habets <thomas@habets.se>
2025-03-04 14:04:48 +0000

    VecToStream: Add start end end tags to output

--- src/vec_to_stream.rs
+++ src/vec_to_stream.rs
@@ -2,11 +2,22 @@
 
 Turn stream of e.g. `Vec<u8>` to stream of `u8`.
  */
+use log::trace;
+
 use crate::Error;
 use crate::block::{Block, BlockRet};
-use crate::stream::{NCReadStream, WriteStream};
+use crate::stream::{NCReadStream, Tag, TagValue, WriteStream};
+
+const TAG_START: &str = "VecToStream::start";
+const TAG_END: &str = "VecToStream::end";
 
 /// Block for vector to stream.
+///
+/// The output stream is tagged with `VecToStream::start` and `VecToStream::end`
+/// on the first and last sample of the stream. For a one-sample vector, these
+/// tags will be on the same sample.
+///
+/// Empty vectors are silently discarded.
 #[derive(rustradio_macros::Block)]
 #[rustradio(crate, new)]
 pub struct VecToStream<T> {
@@ -30,9 +41,71 @@ impl<T: Copy> Block for VecToStream<T> {
             .src
             .pop()
             .expect("we just checked the size. It must exist");
-        // TODO: write start and end tags.
+        debug_assert_eq!(v.len(), n);
+        if n == 0 {
+            trace!("VecToStream: discarded empty vector");
+            return Ok(BlockRet::Ok);
+        }
         o.fill_from_iter(v);
-        o.produce(n, &[]);
+        o.produce(
+            n,
+            &[
+                Tag::new(0, TAG_START.to_string(), TagValue::U64(n as u64)),
+                Tag::new(n - 1, TAG_END.to_string(), TagValue::U64(n as u64)),
+            ],
+        );
         Ok(BlockRet::Ok)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::stream::new_nocopy_stream;
+    use anyhow::Result;
+
+    #[test]
+    fn empty_input() -> Result<()> {
+        let (_tx, rx) = new_nocopy_stream();
+        let (mut b, out) = VecToStream::<u8>::new(rx);
+        assert!(matches![b.work()?, BlockRet::WaitForStream(_, 1)]);
+        assert_eq!(out.read_buf()?.0.len(), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn empty_vec() -> Result<()> {
+        let (tx, rx) = new_nocopy_stream();
+        tx.push(vec![], &[]);
+        let (mut b, out) = VecToStream::<u8>::new(rx);
+        assert!(matches![b.work()?, BlockRet::Ok]);
+        assert!(matches![b.work()?, BlockRet::WaitForStream(_, 1)]);
+        assert_eq!(out.read_buf()?.0.len(), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn two() -> Result<()> {
+        let (tx, rx) = new_nocopy_stream();
+        tx.push(vec![11, 22, 33], &[]);
+        tx.push(vec![3, 2, 1, 0], &[]);
+        let (mut b, out) = VecToStream::<u8>::new(rx);
+        assert!(matches![b.work()?, BlockRet::Ok]);
+        assert_eq!(out.read_buf()?.0.len(), 3);
+        assert!(matches![b.work()?, BlockRet::Ok]);
+        assert_eq!(out.read_buf()?.0.len(), 7);
+        assert!(matches![b.work()?, BlockRet::WaitForStream(_, 1)]);
+        let (o, tags) = out.read_buf()?;
+        assert_eq!(o.len(), 7);
+        assert_eq!(
+            tags,
+            &[
+                Tag::new(0, "VecToStream::start".to_string(), TagValue::U64(3)),
+                Tag::new(2, "VecToStream::end".to_string(), TagValue::U64(3)),
+                Tag::new(3, "VecToStream::start".to_string(), TagValue::U64(4)),
+                Tag::new(6, "VecToStream::end".to_string(), TagValue::U64(4)),
+            ]
+        );
+        Ok(())
     }
 }
