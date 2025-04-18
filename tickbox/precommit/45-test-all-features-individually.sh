ca37a277f4087fa539548cf22c5323f37b4bc123
Thomas Habets <thomas@habets.se>
2025-04-18 17:34:40 +0100

    precommit: Test all features individually

---
+++ tickbox/precommit/45-test-all-features-individually.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -uoe pipefail
+for feature in rtlsdr soapysdr fast-math audio fftw simd; do
+        export CARGO_TARGET_DIR="$TICKBOX_CWD/target/${TICKBOX_BRANCH}.test.feature.${feature}"
+        cargo +nightly test -F "${feature}"
+done
