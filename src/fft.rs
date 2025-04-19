886dec09f607ea6b9f2255c9d31fd0a04df4f99a
Thomas Habets <thomas@habets.se>
2025-04-19 19:27:10 +0100

    Fft: Actually do the FFT

--- src/fft.rs
+++ src/fft.rs
@@ -1,7 +1,10 @@
+use std::sync::Arc;
+
 use crate::block::{Block, BlockRet};
 use crate::stream::{NCReadStream, NCWriteStream};
-use crate::{Complex, Result};
+use crate::{Complex, Float, Result};
 
+/// Run FFT on message.
 #[derive(rustradio_macros::Block)]
 #[rustradio(crate, new)]
 pub struct Fft {
@@ -9,12 +12,54 @@ pub struct Fft {
     src: NCReadStream<Vec<Complex>>,
     #[rustradio(out)]
     dst: NCWriteStream<Vec<Complex>>,
+    fft: Arc<dyn rustfft::Fft<Float>>,
+}
+
+impl Fft {
+    fn process_one(&mut self, input: &[Complex]) -> Vec<Complex> {
+        let mut out = input.to_vec();
+        self.fft.process(&mut out);
+        out
+    }
 }
 
 impl Block for Fft {
     fn work(&mut self) -> Result<BlockRet> {
-        _ = self.src;
-        _ = self.dst;
-        todo!()
+        loop {
+            let Some((msg, tags)) = self.src.pop() else {
+                return Ok(BlockRet::WaitForStream(&self.src, 1));
+            };
+            let out = self.process_one(&msg);
+            self.dst.push(out, &tags);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::stream::new_nocopy_stream;
+
+    #[test]
+    fn zeroes() -> Result<()> {
+        let mut planner = rustfft::FftPlanner::new();
+        let size = 1024;
+        let fft = planner.plan_fft_forward(size);
+        let (root, r) = new_nocopy_stream();
+        let (mut f, out) = Fft::new(r, fft);
+        assert!(matches![out.pop(), None]);
+        assert!(matches![f.work()?, BlockRet::WaitForStream(_, 1)]);
+        assert!(matches![out.pop(), None]);
+        root.push(vec![Complex::default(); size], &[]);
+        assert!(matches![f.work()?, BlockRet::WaitForStream(_, 1)]);
+        // Get the results.
+        let (omsg, tags) = out.pop().unwrap();
+        assert_eq!(omsg.len(), size);
+        assert_eq!(omsg, vec![Complex::default(); size]);
+        assert_eq!(tags, &[]);
+
+        // Should be empty now.
+        assert!(matches![out.pop(), None]);
+        Ok(())
     }
 }
