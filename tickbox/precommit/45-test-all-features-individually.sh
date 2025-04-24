689cbfa2ee7ca588f04bf0d593af94c2f4aeb761
Thomas Habets <thomas@habets.se>
2025-04-24 20:44:39 +0100

    precommit/all features individuall: set correct working dir

--- tickbox/precommit/45-test-all-features-individually.sh
+++ tickbox/precommit/45-test-all-features-individually.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -uoe pipefail
+cd "$TICKBOX_TEMPDIR/work"
 for feature in rtlsdr soapysdr fast-math audio fftw simd; do
         export CARGO_TARGET_DIR="$TICKBOX_CWD/target/${TICKBOX_BRANCH}.test.feature.${feature}"
         cargo +nightly test -F "${feature}"
