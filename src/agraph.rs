717a2b9cc63ff2cca722897854e83845e2a18ba6
Thomas Habets <thomas@habets.se>
2025-05-15 22:19:31 +0100

    WIP: remove old run func

--- src/agraph.rs
+++ src/agraph.rs
@@ -109,22 +109,6 @@ impl GraphRunner for AsyncGraph {
     }
 }
 
-pub async fn run(mut b: Box<dyn Block>) -> Result<()> {
-    loop {
-        let ret = b.work()?;
-        match ret {
-            BlockRet::Again => {}
-            BlockRet::Pending => {}
-            BlockRet::WaitForFunc(_) => {}
-            BlockRet::WaitForStream(stream, need) => {
-                stream.wait_async(need).await;
-            }
-            BlockRet::EOF => break,
-        }
-    }
-    Ok(())
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
