cacd8a1d5bb2de46ff0651fbe8fbd41461f93759
Thomas Habets <thomas@habets.se>
2025-05-17 13:33:37 +0100

    bump_version: don't run precommit when committing version

--- tickbox/bump_version/80-commit-version.sh
+++ tickbox/bump_version/80-commit-version.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 VERSION="$(awk '/^version/ {print $3}' Cargo.toml | head -1 | sed 's/"//g')"
-exec git commit -a -m"Bump version to ${VERSION}"
+exec git commit -nam"Bump version to ${VERSION}"
