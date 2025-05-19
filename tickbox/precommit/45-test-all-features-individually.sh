1fdd84b6e10f789f060eb336e455af16ce03b17e
Thomas Habets <thomas@habets.se>
2025-05-19 19:54:43 +0100

    precommit: allow skipping slowest test

--- tickbox/precommit/45-test-all-features-individually.sh
+++ tickbox/precommit/45-test-all-features-individually.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -uoe pipefail
+if [[ $FAST = "true" ]]; then
+        exit 0
+fi
 cd "$TICKBOX_TEMPDIR/work"
 for feature in rtlsdr soapysdr fast-math audio fftw simd async; do
         export CARGO_TARGET_DIR="$TICKBOX_CWD/target/${TICKBOX_BRANCH}.test.feature.${feature}"
