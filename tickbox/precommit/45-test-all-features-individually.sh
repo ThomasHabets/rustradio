8e7be893a699d5609daf3694215491e39a279966
Thomas Habets <thomas@habets.se>
2025-05-22 19:45:55 +0100

    precommit: Use profile nodebug to speed things up

--- tickbox/precommit/45-test-all-features-individually.sh
+++ tickbox/precommit/45-test-all-features-individually.sh
@@ -6,5 +6,5 @@ fi
 cd "$TICKBOX_TEMPDIR/work"
 for feature in rtlsdr soapysdr fast-math audio fftw simd async; do
         export CARGO_TARGET_DIR="$TICKBOX_CWD/target/${TICKBOX_BRANCH}.test.feature.${feature}"
-        cargo +nightly test -F "${feature}"
+        cargo +nightly test --profile nodebug -F "${feature}"
 done
