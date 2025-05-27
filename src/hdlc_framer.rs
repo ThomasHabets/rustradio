dffc0972031e6c050357cb5ac98251f01b6a6bf4
Thomas Habets <thomas@habets.se>
2025-05-27 22:13:50 +0100

    HdlcFramer: Implement, maybe correctly

--- src/hdlc_framer.rs
+++ src/hdlc_framer.rs
@@ -10,6 +10,8 @@ use crate::Result;
 use crate::block::{Block, BlockRet};
 use crate::stream::{NCReadStream, NCWriteStream};
 
+const SYNC: &[bool] = &[false, true, true, true, true, true, true, false];
+
 /// HDLC framer.
 ///
 /// Takes a packet of bytes, and outputs a packet of bits.
@@ -25,14 +27,80 @@ pub struct HdlcFramer {
     dst: NCWriteStream<Vec<bool>>,
 }
 
+// TODO: confirm that I didn't get the bit order backwards.
+fn hdlc_encode(data: &[u8]) -> Vec<bool> {
+    let mut out = Vec::with_capacity(data.len() * 8 + 32);
+    out.extend(SYNC);
+    let mut ones = 0;
+    for mut byte in data.iter().copied() {
+        for _ in 0..8 {
+            if byte & 1 == 1 {
+                ones += 1;
+                out.push(true);
+                if ones == 5 {
+                    ones = 0;
+                    out.push(false);
+                }
+            } else {
+                ones = 0;
+                out.push(false);
+            }
+            byte >>= 1;
+        }
+    }
+    out.extend(SYNC);
+    out
+}
+
 impl Block for HdlcFramer {
     fn work(&mut self) -> Result<BlockRet> {
-        let Some((x, tags)) = self.src.pop() else {
-            return Ok(BlockRet::WaitForStream(&self.src, 1));
-        };
-        let _ = x;
-        let _ = tags;
-        let _ = &self.dst;
-        todo!()
+        loop {
+            let Some((x, tags)) = self.src.pop() else {
+                return Ok(BlockRet::WaitForStream(&self.src, 1));
+            };
+            let out = hdlc_encode(&x);
+            self.dst.push(out, tags);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn bools_to_string(bs: &[bool]) -> String {
+        let mut s = String::new();
+        for b in bs {
+            s += if *b { "1" } else { "0" };
+        }
+        s
+    }
+
+    #[test]
+    fn various() {
+        let pairs: &[(&[u8], &str)] = &[
+            (b"", ""),
+            (b"\x00", "00000000"),
+            (b"\x00\x55", "0000000010101010"),
+            (b"\x00\xff\x55", "0000000011111011110101010"),
+            (b"\x00\xff\xff", "000000001111101111101111101"),
+            (b"\x00\xff\xff\xff", "000000001111101111101111101111101111"),
+        ];
+        for (i, o) in pairs {
+            let want: Vec<_> = SYNC
+                .iter()
+                .copied()
+                .chain(o.chars().map(|b| b == '1'))
+                .chain(SYNC.iter().copied())
+                .collect();
+            let got = hdlc_encode(&*i);
+            assert_eq!(
+                got,
+                want,
+                "\nwant: {}\ngot:  {}",
+                bools_to_string(&want),
+                bools_to_string(&got)
+            );
+        }
     }
 }
