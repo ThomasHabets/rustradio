3cfbf5b901de2a361ca3ce6f7ab0e4508a1a7f4f
Thomas Habets <thomas@habets.se>
2025-11-20 22:54:48 +0000

    Add volk as an optional dependency
    For a test workload:
    * Before: 32.460s
    * After, but without volk: 26.417s
    * After, with volk: 24.500s
    
    Presumably even without volk the compiler gets a better chance to
    optimize.

--- src/quadrature_demod.rs
+++ src/quadrature_demod.rs
@@ -1,7 +1,8 @@
 //! Quadrature demod, the core of an FM demodulator.
 
+use crate::block::{Block, BlockRet};
 use crate::stream::{ReadStream, WriteStream};
-use crate::{Complex, Float};
+use crate::{Complex, Float, Result};
 
 /// Quadrature demod, the core of an FM demodulator.
 ///
@@ -29,27 +30,84 @@ use crate::{Complex, Float};
 ///
 /// [vectorized]: https://mazzo.li/posts/vectorized-atan2.html
 #[derive(rustradio_macros::Block)]
-#[rustradio(crate, new, sync)]
+#[rustradio(crate, new)]
 pub struct QuadratureDemod {
     gain: Float,
-    #[rustradio(default)]
-    last: Complex,
     #[rustradio(in)]
     src: ReadStream<Complex>,
     #[rustradio(out)]
     dst: WriteStream<Float>,
+
+    #[rustradio(default)]
+    tmp: Vec<Complex>,
 }
 
-impl QuadratureDemod {
-    fn process_sync(&mut self, s: Complex) -> Float {
-        let t = s * self.last.conj();
-        self.last = s;
+impl Block for QuadratureDemod {
+    fn work(&mut self) -> Result<BlockRet<'_>> {
+        let (inp, _) = self.src.read_buf()?;
+        if inp.len() < 2 {
+            return Ok(BlockRet::WaitForStream(&self.src, 2));
+        }
+        let mut out = self.dst.write_buf()?;
+        if out.is_empty() {
+            return Ok(BlockRet::WaitForStream(&self.dst, 1));
+        }
+        let n = inp.len().min(out.len());
+        let n1 = n - 1;
+        let o = &mut out.slice()[..n1];
+        let i = &inp.slice()[..n];
+        if self.tmp.len() < n {
+            self.tmp.resize(n, Complex::default());
+        }
 
+        // Conjugate.
+
+        #[cfg(feature = "volk")]
+        volk::volk_32fc_x2_multiply_conjugate_32fc(&mut self.tmp[..n1], &i[1..n], &i[..n1]);
+        #[cfg(not(feature = "volk"))]
+        {
+            for t in 0..n1 {
+                self.tmp[t] = i[t].conj() * i[t + 1];
+            }
+        }
+
+        // atan2
         #[cfg(feature = "fast-math")]
-        return self.gain * fast_math::atan2(t.im, t.re);
-
+        {
+            for (i, item) in o.iter_mut().enumerate().take(n1) {
+                *item = self.gain * fast_math::atan2(self.tmp[i].im, self.tmp[i].re);
+            }
+            if false {
+                // Maybe this can be faster in some circumstances, but not yet in my
+                // testing.
+                use rayon::iter::IndexedParallelIterator;
+                use rayon::iter::IntoParallelRefIterator;
+                use rayon::iter::IntoParallelRefMutIterator;
+                use rayon::iter::ParallelIterator;
+                o.par_iter_mut()
+                    .zip(self.tmp.par_iter())
+                    .for_each(|(a, b)| {
+                        *a = self.gain * fast_math::atan2(b.im, b.re);
+                    });
+            }
+        }
         #[cfg(not(feature = "fast-math"))]
-        return self.gain * t.im.atan2(t.re);
+        {
+            // This is way slower than fast-math. Fast-math atan2 is just that
+            // fast. Maybe one day it'll be in volk.
+            //
+            // https://mazzo.li/posts/vectorized-atan2.html
+            #[cfg(feature = "volk")]
+            volk::volk_32fc_s32f_atan2_32f(&mut out.slice()[..n1], &self.tmp[..n1], self.gain);
+
+            #[cfg(not(feature = "volk"))]
+            o.iter_mut().zip(self.tmp.iter()).for_each(|(a, b)| {
+                *a = self.gain * b.im.atan2(b.re);
+            });
+        }
+        inp.consume(n1);
+        out.produce(n1, &[]);
+        Ok(BlockRet::Pending)
     }
 }
 
@@ -101,5 +159,68 @@ impl FastFM {
         self.q2 = self.q1;
         self.q1 = s;
         top - bottom
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::blocks::VectorSource;
+    use crate::tests::assert_almost_equal_float;
+
+    #[test]
+    fn quad_nulls() -> Result<()> {
+        let (mut b, prev) = VectorSource::new(vec![Complex::default(); 4]);
+        b.work()?;
+        let (mut b, out) = QuadratureDemod::new(prev, 1.0);
+        b.work()?;
+        let (o, _) = out.read_buf()?;
+        assert_eq!(o.slice(), vec![0.0f32; 3]);
+        Ok(())
+    }
+
+    #[test]
+    fn quad_cw() -> Result<()> {
+        let (mut b, prev) = VectorSource::new(vec![
+            Complex::new(1.0, 0.0),
+            Complex::new(0.707, -0.707),
+            Complex::new(0.0, -1.0),
+            Complex::new(-1.0, 0.0),
+        ]);
+        b.work()?;
+        let (mut b, out) = QuadratureDemod::new(prev, 1.0);
+        b.work()?;
+        let (o, _) = out.read_buf()?;
+        assert_almost_equal_float(
+            o.slice(),
+            &[
+                -std::f32::consts::PI / 4.0,
+                -std::f32::consts::PI / 4.0,
+                -std::f32::consts::PI / 2.0,
+            ],
+        );
+        Ok(())
+    }
+    #[test]
+    fn quad_ccw() -> Result<()> {
+        let (mut b, prev) = VectorSource::new(vec![
+            Complex::new(1.0, 0.0),
+            Complex::new(0.707, 0.707),
+            Complex::new(0.0, 1.0),
+            Complex::new(-1.0, 0.0),
+        ]);
+        b.work()?;
+        let (mut b, out) = QuadratureDemod::new(prev, 1.0);
+        b.work()?;
+        let (o, _) = out.read_buf()?;
+        assert_almost_equal_float(
+            o.slice(),
+            &[
+                std::f32::consts::PI / 4.0,
+                std::f32::consts::PI / 4.0,
+                std::f32::consts::PI / 2.0,
+            ],
+        );
+        Ok(())
     }
 }
