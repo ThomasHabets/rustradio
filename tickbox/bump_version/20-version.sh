bb26b46ca43fba9c748c1b4518df20eca20fb90b
Thomas Habets <thomas@habets.se>
2025-11-21 23:08:57 +0000

    bump_version: Get manual version from env, not cmdline

--- tickbox/bump_version/20-version.sh
+++ tickbox/bump_version/20-version.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 CURRENT="$(awk '/^version/ {print $3}' Cargo.toml | head -1 | sed 's/"//g')"
 AUTO_NEW_VERSION="$(echo $CURRENT | awk -F. '{print $1 "." $2 "." $3+1}')"
-NEW="${1:-$AUTO_NEW_VERSION}"
+NEW="${BUMP_NEW_VERSION:-$AUTO_NEW_VERSION}"
 
 # MANUAL_VERSION is set manually by the person running the release.
 NEW="${MANUAL_VERSION:-$NEW}"
