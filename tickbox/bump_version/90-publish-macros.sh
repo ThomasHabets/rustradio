403a1faa3f94d8b64695bc014cae3020763d776b
Thomas Habets <thomas@habets.se>
2025-11-30 16:36:24 +0000

    bump_version: Fix cargo publish loop

--- tickbox/bump_version/90-publish-macros.sh
+++ tickbox/bump_version/90-publish-macros.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 for dir in rustradio_macros_code rustradio_macros; do
-        cargo publish
+        (
+                cd "$dir"
+                cargo publish
+        )
 done
