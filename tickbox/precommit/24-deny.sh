5194d6960878fc23960daab0d6be6edb13a542d4
Thomas Habets <thomas@habets.se>
2025-12-05 19:09:13 +0000

    precommit: skip cargo deny if no network

--- tickbox/precommit/24-deny.sh
+++ tickbox/precommit/24-deny.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -ueo pipefail
+if [[ "$NO_NET" = "true" ]]; then
+        exit 0
+fi
 cd "$TICKBOX_TEMPDIR/work"
 export CARGO_TARGET_DIR="$TICKBOX_CWD/target/${TICKBOX_BRANCH}.deny"
 exec cargo deny check
