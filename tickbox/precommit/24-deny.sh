a07c3c5bc98619de3f4a1dca3c0071d0d72231ee
Thomas Habets <thomas@habets.se>
2025-12-06 22:35:43 +0000

    precommit: enable all features with cargo deny check

--- tickbox/precommit/24-deny.sh
+++ tickbox/precommit/24-deny.sh
@@ -5,6 +5,4 @@ if [[ "${NO_NET:-}" = "true" ]]; then
 fi
 cd "$TICKBOX_TEMPDIR/work"
 export CARGO_TARGET_DIR="$TICKBOX_CWD/target/${TICKBOX_BRANCH}.deny"
-exec cargo deny check
-# Was, and maybe should at some point be changed back to:
-# exec cargo clippy --all-features --all-targets
+exec cargo deny --all-features --workspace check
