a3cfc3f98b775c14612e4af27a89b0a401c48d56
Thomas Habets <thomas@habets.se>
2026-05-24 21:24:27 +0100

    examples/rtl_data_stream: Make the sink block more general

--- examples/rtl_data_stream.rs
+++ examples/rtl_data_stream.rs
@@ -9,7 +9,8 @@ use std::thread::JoinHandle;
 use anyhow::{Result, bail};
 use clap::Parser;
 
-use rustradio::block::{Block, BlockEOF, BlockName, BlockRet};
+use rustradio::Error;
+use rustradio::block::{Block, BlockRet};
 use rustradio::blockchain;
 use rustradio::blocks::*;
 use rustradio::data_stream::{DataStreamId, Packet, RequestData, SyncReader, SyncWriter};
@@ -22,6 +23,14 @@ struct Opt {
     /// Tuned frequency, if reading from RTL SDR.
     #[arg(long = "freq", default_value_t = 100_000_000)]
     freq: u64,
+
+    /// Sample rate.
+    #[arg(long, short, default_value_t = 250_000)]
+    sample_rate: u32,
+
+    /// Sample rate.
+    #[arg(long, short, default_value_t = 50_000)]
+    downsample_rate: u32,
 
     /// Verbosity of debug messages.
     #[arg(short, default_value = "0")]
@@ -40,59 +49,52 @@ struct Opt {
     packet_bytes: usize,
 }
 
+/// A sink that writes data to a DataStream, probably a websocket heading for
+/// the UI.
+#[derive(rustradio_macros::Block)]
+#[rustradio(new)]
 struct DataStreamSink<W> {
+    #[rustradio(in)]
     src: ReadStream<u8>,
     writer: SyncWriter<W>,
-    control: Receiver<RequestData>,
     stream_id: DataStreamId,
-    window: usize,
     max_packet_data: usize,
+
+    #[rustradio(default)]
+    control: Option<Receiver<RequestData>>,
+    #[rustradio(default)]
+    window: usize,
 }
 
 impl<W> DataStreamSink<W>
 where
     W: Write + Send,
 {
-    #[must_use]
-    fn new(
-        src: ReadStream<u8>,
-        writer: SyncWriter<W>,
-        control: Receiver<RequestData>,
-        stream_id: DataStreamId,
-        max_packet_data: usize,
-    ) -> Self {
-        Self {
-            src,
-            writer,
-            control,
-            stream_id,
-            window: 0,
-            max_packet_data,
+    /// Get control channel.
+    pub fn control(&mut self) -> Result<mpsc::Sender<RequestData>> {
+        if self.control.is_some() {
+            return Err(Error::msg("DataStreamSink::control called twice").into());
         }
+        let (tx, rx) = mpsc::channel();
+        self.control = Some(rx);
+        Ok(tx)
     }
 
+    // Check for a new window size.
     fn update_window(&mut self) {
-        while let Ok(req) = self.control.try_recv() {
+        let Some(ref control) = self.control else {
+            return;
+        };
+        while let Ok(req) = control.try_recv() {
             if req.stream_id == self.stream_id {
                 self.window = req.window;
             }
         }
     }
 
+    // Send requested data on the connection.
     fn write_data_packet(&mut self, data: &[u8]) -> rustradio::Result<()> {
         self.writer.write_data(&self.stream_id, data)
-    }
-}
-
-impl<W> BlockName for DataStreamSink<W> {
-    fn block_name(&self) -> &str {
-        "DataStreamSink"
-    }
-}
-
-impl<W> BlockEOF for DataStreamSink<W> {
-    fn eof(&mut self) -> bool {
-        self.src.eof()
     }
 }
 
@@ -101,28 +103,40 @@ where
     W: Write + Send,
 {
     fn work(&mut self) -> rustradio::Result<BlockRet<'_>> {
-        self.update_window();
-        if self.window == 0 {
-            return Ok(BlockRet::Pending);
-        }
+        loop {
+            // First clear any pending control messages.
+            self.update_window();
 
-        let (input, _tags) = self.src.read_buf()?;
-        if input.is_empty() {
-            return Ok(BlockRet::WaitForStream(&self.src, 2));
-        }
+            // Then check for input, since input can be slept on.
+            let (input, _tags) = self.src.read_buf()?;
+            if input.is_empty() {
+                return Ok(BlockRet::WaitForStream(&self.src, 2));
+            }
 
-        let n = input.len().min(self.window).min(self.max_packet_data) & !1usize;
-        if n == 0 {
-            return Ok(BlockRet::Pending);
-        }
+            // Window updates can currently not be waited for. TODO: is there a
+            // way we could make it pending on the control channel?
+            if self.window == 0 {
+                return Ok(BlockRet::Pending);
+            }
 
-        self.write_data_packet(&input.slice()[..n])?;
-        input.consume(n);
-        self.window -= n;
-        Ok(BlockRet::Again)
+            let n = input.len().min(self.window).min(self.max_packet_data);
+            debug_assert_ne!(
+                n, 0,
+                "this should not be possible. We just checked it! Unless max packet data ({}) is 0?",
+                self.max_packet_data
+            );
+            if n == 0 {
+                return Ok(BlockRet::Pending);
+            }
+
+            self.write_data_packet(&input.slice()[..n])?;
+            input.consume(n);
+            self.window -= n;
+        }
     }
 }
 
+// Reader side of the websocket reader.
 fn spawn_control_reader(
     control: mpsc::Sender<RequestData>,
     cancel: CancellationToken,
@@ -162,26 +176,24 @@ fn run(opt: Opt) -> Result<()> {
         bail!("--packet-bytes must be at least 2");
     }
 
-    let samp_rate = 250_000;
-    let samp_rate_2 = 50_000;
+    let samp_rate = opt.sample_rate;
+    let samp_rate_2 = opt.downsample_rate;
     let stdout = std::io::BufWriter::new(std::io::stdout());
     let mut writer = SyncWriter::new(stdout);
     writer.write_version()?;
 
-    let (control_tx, control_rx) = mpsc::channel();
     let mut g = Graph::new();
-    let _control_thread = spawn_control_reader(control_tx, g.cancel_token());
     let prev = blockchain![
         g,
         prev,
-        RtlSdrSource::new(opt.freq, samp_rate as u32, opt.gain)?,
+        RtlSdrSource::new(opt.freq, samp_rate, opt.gain)?,
         RtlSdrDecode::new(prev),
         FftFilter::new(
             prev,
             rustradio::fir::low_pass_complex(
                 samp_rate as f32,
-                40_000.0,
-                1_000.0,
+                (opt.downsample_rate as f32) * 0.8,
+                1_000.0, // Sharp filter.
                 &rustradio::window::WindowType::Hamming,
             )
         ),
@@ -191,13 +203,10 @@ fn run(opt: Opt) -> Result<()> {
             .build(prev)?,
         RtlSdrEncode::new(prev),
     ];
-    g.add(Box::new(DataStreamSink::new(
-        prev,
-        writer,
-        control_rx,
-        opt.stream_id,
-        opt.packet_bytes,
-    )));
+    let mut sink = DataStreamSink::new(prev, writer, opt.stream_id, opt.packet_bytes);
+    let control_tx = sink.control()?;
+    g.add(Box::new(sink));
+    let _control_thread = spawn_control_reader(control_tx, g.cancel_token());
     Ok(g.run()?)
 }
 
