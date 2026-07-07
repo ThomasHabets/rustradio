e27b3e54eecaac6845069a484def93054db429c8
Thomas Habets <thomas@habets.se>
2026-07-07 22:13:32 +0100

    UI: Move shared CSS to rustradio-ui asset

--- rustradio-ui/examples/rtlsdr-fm/build-local.sh
+++ rustradio-ui/examples/rtlsdr-fm/build-local.sh
@@ -13,10 +13,10 @@ wasm-pack build --target web -d "$TMPD/$PREFIX" "--$PROFILE"
 GIT="$(git describe --tags --dirty --always)"
 cp \
         "$WEBD/index.html" \
-        "$WEBD/style.css" \
         "$WEBD/wasm-mod.js" \
         "$TMPD/$PREFIX/"
 cp "$UI_ASSETS/bootstrap.js" "$TMPD/$PREFIX/rustradio-ui-bootstrap.js"
+cat "$UI_ASSETS/rustradio.css" > "$TMPD/$PREFIX/style.css"
 
 sed -i "s/GIT_VERSION_NOT_SET/$GIT/g" "$TMPD/$PREFIX/index.html"
 (
